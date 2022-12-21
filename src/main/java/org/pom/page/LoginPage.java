83bca2ca0cf9d8fd94f91f1cfacc21178861f01c
Jana3037 <janarthananr94@gmail.com>
2022-12-21 23:35:58 +0530

    new code suresh

--- src/main/java/org/pom/page/LoginPage.java
+++ src/main/java/org/pom/page/LoginPage.java
@@ -5,6 +5,10 @@ public class LoginPage {
 	
 	public static void main(String[] args) {
 		System.out.println("78954562123");
+		System.out.println("qwertyuioasdfghjzxcvbn");
+		System.out.println("sdfg74196");
+		System.out.println("sdfghyhb85263741");
+		System.out.println("test");
 	}
 	
 	
