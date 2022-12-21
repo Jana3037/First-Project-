56448e39ade9a00b7809aadf76eb8ea7d224ef5b
Jana3037 <janarthananr94@gmail.com>
2022-12-21 23:46:19 +0530

    Ramesh code 2

--- src/main/java/org/pom/page/LoginPage.java
+++ src/main/java/org/pom/page/LoginPage.java
@@ -5,6 +5,10 @@ public class LoginPage {
 	
 	public static void main(String[] args) {
 		System.out.println("78954562123");
+		System.out.println("kertyhbjk,l");
+		System.out.println("1234566777");
+		System.out.println("4151411");
+		System.out.println("ala7d5adsf45dfsa");
 	}
 	
 	
